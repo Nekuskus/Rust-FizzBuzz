e883f798a98d127f609e244b28063a26676a098c
YUKI.N <61391965+Nekuskus@users.noreply.github.com>
2023-12-02 17:25:57 +0100

    Complete "FizzBuzz."

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", (1..101).into_iter().map(|num| {
+        match num {
+            _ if num % 15 == 0 => String::from("FizzBuzz"),
+            _ if num % 5 == 0 => String::from("Buzz"),
+            _ if num % 3 == 0 => String::from("Fizz"),
+            _ => num.to_string()
+        }
+    }).collect::<Vec<String>>().join(" "));
 }
